13109f266555c30d6e0e63dd6ff25841bfd73fe5
waleedkhano <khanow99@gmail.com>
2023-12-07 01:21:55 +0500

    nav and footer responsive

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -11,7 +11,7 @@ export const Footer = () => {
           <div className="logo">
             <img src="https://demo.phlox.pro/freelancer/wp-content/uploads/sites/272/2021/02/logo-header.svg" alt="logo" />
           </div>
-          <p>© 2023 Freelancer - Phlox Elementor WordPress Theme. All rights reserved.</p>
+          <p className='footerpara'>© 2023 Freelancer - Phlox Elementor WordPress Theme. All rights reserved.</p>
         <div className="footerIcons">
           <Link className='footerIcon' target="_blank" href={'/portfolio'}>F</Link>
           <Link className='footerIcon' target="_blank" href={'/portfolio'}><FaLinkedinIn /></Link>
--- src/component/nav/Nav.jsx
+++ src/component/nav/Nav.jsx
@@ -1,9 +1,19 @@
+'use client'
 import Link from 'next/link'
 import './nav.css'
 import { RiGlobalLine } from "react-icons/ri";
+import { LiaBarsSolid } from "react-icons/lia";
+import { RxCrossCircled } from "react-icons/rx";
+import { useState } from 'react';
 
 
 export const Nav = () => {
+
+    const [toggle, setToggle] = useState(false);
+    const toggleHandler = () => {
+        setToggle(!toggle)
+    }
+
     return (
         <>
             <div className="navbar">
@@ -11,13 +21,18 @@ export const Nav = () => {
                     <div className="logo">
                         <img src="https://demo.phlox.pro/freelancer/wp-content/uploads/sites/272/2021/02/logo-header.svg" alt="logo" />
                     </div>
-                    <div className="navContent">
+                    <div className={`navContent ${toggle ? 'toggle' : ''}`}>
                         <Link href={'/'}>Home</Link>
                         <Link href={'/portfolio'}>Portfolio</Link>
                         <Link href={'/contact'}>Contact</Link>
                     </div>
                 </div>
+               
                 <div className="navRight">
+                <div>
+                    < RxCrossCircled onClick={toggleHandler} className={`cross ${toggle ? 'crossActive' : ''}`}/>
+                <LiaBarsSolid onClick={toggleHandler} className='barIcon' />
+                </div>
                     <Link className='navIcon'  href={'/contact'}><RiGlobalLine /></Link>
                     <Link className='navContactBtn' href={'/contact'}>Contact Me</Link>
                 </div>
