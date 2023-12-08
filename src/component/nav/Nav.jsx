076dfc1fc8062daf9d322c60a9c8a2ead157e7e2
waleedkhano <khanow99@gmail.com>
2023-12-08 20:36:39 +0500

    nav animation

--- src/component/nav/Nav.jsx
+++ src/component/nav/Nav.jsx
@@ -4,15 +4,40 @@ import './nav.css'
 import { RiGlobalLine } from "react-icons/ri";
 import { LiaBarsSolid } from "react-icons/lia";
 import { RxCrossCircled } from "react-icons/rx";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import gsap from 'gsap';
 
 
 export const Nav = () => {
 
     const [toggle, setToggle] = useState(false);
+    const [activeLink, setActiveLink] = useState('/');
     const toggleHandler = () => {
         setToggle(!toggle)
     }
+
+    useEffect(() => {
+        var tl = gsap.timeline({ paused: true });
+
+        tl.to(".logo", 1, {
+            x: 50,
+            opacity: 1,
+        }).to(".navContent", {
+            opacity: 1,
+        }, "same").to(".navIcon", {
+            x: -50,
+            opacity: 1,
+        }, "same").to(".navContactBtn", 0.8, {
+            x: -50,
+            opacity: 1,
+        })
+        tl.play();
+    }, [toggle])
+
+    useEffect(() => {
+        setActiveLink(window.location.pathname);
+        console.log(activeLink)
+    }, []);
 
     return (
         <>
@@ -22,19 +47,18 @@ export const Nav = () => {
                         <img src="https://demo.phlox.pro/freelancer/wp-content/uploads/sites/272/2021/02/logo-header.svg" alt="logo" />
                     </div>
                     <div className={`navContent ${toggle ? 'toggle' : ''}`}>
-                        <Link href={'/'}>Home</Link>
-                        <Link href={'/portfolio'}>Portfolio</Link>
-                        <Link href={'/contact'}>Contact</Link>
-                    </div>
+                        <a href={'/'} className={activeLink === '/' ? 'active' : ''}>Home</a>
+                        <a href={'/portfolio'} className={activeLink === '/portfolio' ? 'active' : ''}>Portfolio</a>
+                        <a href={'/contact'} className={activeLink === '/contact' ? 'active' : ''}>Contact</a>                    </div>
                 </div>
-               
+
                 <div className="navRight">
-                <div>
-                    < RxCrossCircled onClick={toggleHandler} className={`cross ${toggle ? 'crossActive' : ''}`}/>
-                <LiaBarsSolid onClick={toggleHandler} className='barIcon' />
-                </div>
-                    <Link className='navIcon'  href={'/contact'}><RiGlobalLine /></Link>
-                    <Link className='navContactBtn' href={'/contact'}>Contact Me</Link>
+                    <div>
+                        < RxCrossCircled onClick={toggleHandler} className={`cross ${toggle ? 'crossActive' : ''}`} />
+                        <LiaBarsSolid onClick={toggleHandler} className='barIcon' />
+                    </div>
+                    <a className='navIcon' href={'/contact'}><RiGlobalLine /></a>
+                    <a className='navContactBtn' href={'/contact'}>Contact Me</a>
                 </div>
             </div>
         </>
